aca65ced1815bd99e32609272a61c11b74e10749
77920 <779204924@qq.com>
2020-09-02 20:58:18 +0800

    CountDownLatch 中文注释

--- src/java/util/concurrent/ConcurrentHashMap.java
+++ src/java/util/concurrent/ConcurrentHashMap.java
@@ -935,16 +935,16 @@ public class ConcurrentHashMap<K,V> extends AbstractMap<K,V>
      */
     public V get(Object key) {
         Node<K,V>[] tab; Node<K,V> e, p; int n, eh; K ek;
-        int h = spread(key.hashCode());
+        int h = spread(key.hashCode());//获取hash
         if ((tab = table) != null && (n = tab.length) > 0 &&
-            (e = tabAt(tab, (n - 1) & h)) != null) {
+            (e = tabAt(tab, (n - 1) & h)) != null) {//（n-1)&h 的下标有值
             if ((eh = e.hash) == h) {
-                if ((ek = e.key) == key || (ek != null && key.equals(ek)))
+                if ((ek = e.key) == key || (ek != null && key.equals(ek)))//根节点有值，直接取
                     return e.val;
             }
-            else if (eh < 0)
+            else if (eh < 0)//表示正在扩容，那么就从forwardingNode里查找
                 return (p = e.find(h, key)) != null ? p.val : null;
-            while ((e = e.next) != null) {
+            while ((e = e.next) != null) {//遍历查找
                 if (e.hash == h &&
                     ((ek = e.key) == key || (ek != null && key.equals(ek))))
                     return e.val;
